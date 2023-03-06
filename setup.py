73171f91db2cdf1ba776a1dbf9d0dcc9ef0e76da
gnikit <giannis.nikiteas@gmail.com>
2023-03-06 23:12:16 +0000

    build: add cmake wheel build system
    Use cmake and scikit-build to:
    1. Fetch fpm sources and its dependencies
    2. Link to OpenMP
    3. Compile into an executable
    4. Package into a wheel

---
+++ setup.py
@@ -0,0 +1,10 @@
+from skbuild import setup
+
+setup(
+    name="fpm",
+    version="0.7.0",
+    description="Fortran Package Manager",
+    author="Fortran-lang",
+    license="MIT",
+    python_requires=">=3.7",
+)

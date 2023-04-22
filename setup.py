ee6b766adb83d4d3c3d31e79e52afa60ca75e1e7
gnikit <giannis.nikiteas@gmail.com>
2023-04-22 16:00:26 +0100

    ci: move all cibuildwheel settings to toml

--- setup.py
+++ setup.py
@@ -2,15 +2,9 @@ import sys
 
 from skbuild import setup
 
+CMAKE_GENERATOR = "Ninja" if sys.platform != "win32" else "MSYS Makefiles"
+
 setup(
-    name="fpm",
-    version="0.7.0",
-    description="Fortran Package Manager",
-    author="Fortran-lang",
-    license="MIT",
-    python_requires=">=3.7",
-    cmake_args=(
-        ["-G", "MSYS Makefiles"] if sys.platform == "win32" else ["-G", "Ninja"]
-    ),
+    cmake_args=(["-G", CMAKE_GENERATOR]),
     cmake_with_sdist=True,
 )
--- tools/wheels/cibw_before_build_macos.sh
+++ tools/wheels/cibw_before_build_macos.sh
@@ -5,9 +5,9 @@ PLATFORM="macosx-arm64"
 PLAT="arm64"
 source $PROJECT_DIR/tools/wheels/gfortran_utils.sh
 install_gfortran
-pip install "delocate==0.10.4"
 
-echo "@@@ cross-compiler: " $FC_ARM64
-echo "@@@ default compiler(FC): " $FC
-echo "@@@ FC_ARM64_LDFLAGS: " $FC_ARM64_LDFLAGS
-export LDFLAGS="$FC_ARM64_LDFLAGS:$LDFLAGS"
+# Cannot pass these variables to the wheel building process since before-build
+# executes in a subprocess. So we print them out and manually set them in
+# pyproject.toml
+echo "@@@ FC:" $FC
+echo "@@@ FC_ARM64_LDFLAGS:" $FC_ARM64_LDFLAGS

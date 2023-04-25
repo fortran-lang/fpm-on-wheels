190a5335b3c9fa484fea2b14cddbf8a6a0a12fd8
gnikit <giannis.nikiteas@gmail.com>
2023-04-25 06:55:36 +0100

    build: adds `entry_point` support
    Allows fpm to be called as a module i.e.
    
    ```py
    python -m fpm
    ```

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 import sys
 
+from setuptools import find_packages
 from skbuild import setup
 
 CMAKE_GENERATOR = "Ninja" if sys.platform != "win32" else "MSYS Makefiles"
 
 setup(
     cmake_args=(["-G", CMAKE_GENERATOR]),
+    packages=find_packages(
+        where="src",
+    ),
+    package_dir={"": "src"},
+    entry_points={
+        "console_scripts": [
+            "fpm=fpm:main",
+        ],
+    },
     cmake_with_sdist=True,
 )

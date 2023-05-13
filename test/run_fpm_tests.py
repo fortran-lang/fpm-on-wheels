ba7dc00f18636feb944b7fecc31ee0bb0ae32d28
gnikit <giannis.nikiteas@gmail.com>
2023-05-13 23:23:41 +0100

    ci: added execution of fpm test-suite

---
+++ test/run_fpm_tests.py
@@ -0,0 +1,15 @@
+import sys
+from subprocess import run
+import shutil
+
+if len(sys.argv) < 2:
+    print("Usage: python run_test_suite.py <fpm-tag>")
+    sys.exit(1)
+
+tag = sys.argv[1]
+DIR = "fpm-source"
+run(["git", "clone", "https://github.com/fortran-lang/fpm.git", DIR])
+run(["git", "checkout", tag], cwd=DIR)
+run(["fpm", "build"], cwd=DIR)
+run(["fpm", "test"], cwd=DIR)
+shutil.rmtree(DIR)
